3fff020aa77a4bbb8ca3501d9a946fbb5ee17238
Rafaela Borges <rafasborgesw@gmail.com>
2025-01-26 16:48:27 -0300

    feat: s3 - variaveis

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 console.log('Olá, Javascript!')
 console.log('Olá, Rafa!')
+
+
+var UserName = "Rafaela Borges"
+
+document.getElementById('user-name').innerHTML = UserName
+
+// var nome = "Mestre Yoda"
+// var idade = 100
+// var jedi = true
+
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof jedi)
