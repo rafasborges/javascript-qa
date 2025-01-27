cd098b6f1af14d1f8f883e35f8b048ef82119030
Rafaela Borges <rafasborgesw@gmail.com>
2025-01-26 21:25:11 -0300

    feat: s3 - 11, 12, 13

--- js/app.js
+++ js/app.js
@@ -1,16 +1,44 @@
 console.log('Olá, Javascript!')
 console.log('Olá, Rafa!')
 
-
+// Variáveis 
 var UserName = "Rafaela Borges"
-
 document.getElementById('user-name').innerHTML = UserName
-
 // var nome = "Mestre Yoda"
 // var idade = 100
 // var jedi = true
-
-
 // console.log(typeof nome)
 // console.log(typeof idade)
 // console.log(typeof jedi)
+
+
+// Operadores matemáticos
+// var n1 = 5
+// var n2 = 3
+// var total = n1 * n2
+// console.log(total)
+
+
+// Operadores de comparação
+// var v1 = 5
+// var v2 = '5'
+// var resultado = v1 === v2 //levando a tipagem em consideração
+// console.log(resultado)
+// var resultadoSemTipagem = v1 == v2 //não levando a tipagem em consideração
+
+// Funções
+
+// function soma(n1,n2){
+//     return(n1 + n2)
+// }
+// var resultado = soma(5,3)
+// console.log(resultado)
+
+
+// function boasVindas(UserName){
+//     alert( UserName + ", seja bem-vinda!")
+// }
+// boasVindas(UserName)
+
+// Controle de Fluxos
+
